021829c268180788e7c2bef0bd24cb2bc51c456d
Revathi <s190273@rguktsklm.ac.in>
2024-12-30 16:36:13 +0530

    Fix: new update

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,20 +1,35 @@
+import jwt from "jsonwebtoken";
+
+const generateTokenAndSetCookie = (userId, res) => {
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+		expiresIn: "15d",
+	});
+
+	res.cookie("jwt", token, {
+		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
+		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+		secure: process.env.NODE_ENV !== "development",
+	});
+};
+
+export default generateTokenAndSetCookie;
 
 
-import express from "express";
-import generateTokenAndSetCookie from "./utils/generateTokenAndSetCookie"; // Adjust path as necessary
 
-const app = express();
 
-app.post("/login", (req, res) => {
-  const { userId } = req.body; // Assuming the userId is passed in the request body
-  
-  // Generate token and set the cookie
-  generateTokenAndSetCookie(userId, res);
-  
-  return res.status(200).json({ message: "Logged in successfully!" });
-});
 
-// import jwt from "jsonwebtoken";
+
+
+
+
+
+
+
+
+
+// const jwt=require("jsonwebtoken");
+
 
 // const generateTokenAndSetCookie = (userId, res) => {
 // 	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
@@ -27,38 +42,7 @@ app.post("/login", (req, res) => {
 // 		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
 // 		secure: process.env.NODE_ENV !== "development",
 // 	});
+
 // };
 
-// export default generateTokenAndSetCookie;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // const jwt=require("jsonwebtoken");
-
-
-// // const generateTokenAndSetCookie = (userId, res) => {
-// // 	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-// // 		expiresIn: "15d",
-// // 	});
-
-// // 	res.cookie("jwt", token, {
-// // 		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
-// // 		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-// // 		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-// // 		secure: process.env.NODE_ENV !== "development",
-// // 	});
-
-// // };
-
-// // module.exports=generateTokenAndSetCookie;
+// module.exports=generateTokenAndSetCookie;
